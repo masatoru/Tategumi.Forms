744f4075a743e3c52a540116b2df2cc8e06993e3
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-12-20 02:16:59 +0900

    Mac: ViewControllerに描画処理を追加

--- Tategumi.Mac/ViewController.cs
+++ Tategumi.Mac/ViewController.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-
+using System.IO;
 using AppKit;
 using Foundation;
+using SkiaSharp;
 using SkiaSharp.Views.Mac;
 using Tategumi.Core;
+using Tategumi.Models;
+using Tategumi.TategumiViews;
 
 namespace Tategumi.Mac
 {
     public partial class ViewController : NSViewController
     {
+        protected SKMatrix Matrix = SKMatrix.MakeIdentity();
+        public bool IsInitialized { get; private set; } = false;
+        BookManager Manager { get; set; }
         public ViewController(IntPtr handle) : base(handle)
         {
+            Manager = new BookManager();
         }
 
         public override void ViewDidLoad()
@@ -33,6 +40,26 @@ namespace Tategumi.Mac
             }
         }
 
+        public void Init()
+        {
+            // reset the matrix for the new sample
+            Matrix = SKMatrix.MakeIdentity();
+
+            // リソースから読み込む
+            var path = NSBundle.MainBundle.PathForResource("kokoro", "htm");
+            var text = System.IO.File.ReadAllText(path);
+
+            Manager.ReadFromText(text);
+            Manager.TateviewWidth = canvas.CanvasSize.Width;
+            Manager.TateviewHeight = canvas.CanvasSize.Height;
+            path = NSBundle.MainBundle.PathForResource("ipaexm", "ttf");
+            TategumiViewCore.OpenFontStream = () => File.Open(path,FileMode.Open);
+
+            // 組版する
+            Manager.Compose();
+            IsInitialized = true;
+        }
+
         public override void ViewWillAppear()
         {
             base.ViewWillAppear();
@@ -49,9 +76,10 @@ namespace Tategumi.Mac
 
         private void OnPaintCanvas(object sender, SKPaintSurfaceEventArgs e)
         {
-            //OnPaintSurface(e.Surface.Canvas, e.Info.Width, e.Info.Height);
-            TextSample.DrawSample(e.Surface.Canvas, e.Info.Width, e.Info.Height);
-
+            Init();
+            if (Manager.IsValid())
+                TategumiViewCore.DrawHonbunPage(e.Surface.Canvas, Manager.PageList?[0], false);
+            //TextSample.DrawSample(e.Surface.Canvas, e.Info.Width, e.Info.Height);
         }
 
     }
