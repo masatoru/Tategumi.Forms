9c739cfda8d3befe84079fdddf578803ff79104d
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-12-19 22:36:43 +0900

    Forms: Initの場所をとりあえずOnPaintに実装

--- Tategumi.Forms/Tategumi.Forms/MainPage.xaml.cs
+++ Tategumi.Forms/Tategumi.Forms/MainPage.xaml.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Tategumi.Core;
+using Tategumi.Models;
 using Tategumi.Services;
+using Tategumi.TategumiViews;
 using Xamarin.Forms;
 
 namespace Tategumi.Forms
@@ -15,33 +17,44 @@ namespace Tategumi.Forms
     {
         protected SKMatrix Matrix = SKMatrix.MakeIdentity();
         public bool IsInitialized { get; private set; } = false;
+        BookManager Manager { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
-            Init();
+            Manager = new BookManager();
+            //Init();
         }
 
-        /// <summary>
-        /// Read text from path
-        /// </summary>
-        /// <returns>The book from file name.</returns>
-        /// <param name="fileName">File name.</param>
-        public string GetBookFromFileName(string fileName)
+        protected override void OnAppearing()
         {
-            return DependencyService.Get<IResourceDirectory>().ReadText(fileName);
+            base.OnAppearing();
+            //Init();
         }
 
         public void Init()
         {
             // reset the matrix for the new sample
             Matrix = SKMatrix.MakeIdentity();
+
+            // リソースから読み込む
+            var text = DependencyService.Get<IResourceDirectory>().ReadText("kokoro.htm");
+            Manager.ReadFromText(text);
+            Manager.TateviewWidth = canvas.CanvasSize.Width;
+            Manager.TateviewHeight = canvas.CanvasSize.Height;
+            TategumiViewCore.OpenFontStream = 
+                () => DependencyService.Get<IResourceDirectory>().OpenFontFile("ipaexm.ttf");
+
+            // 組版する
+            Manager.Compose();
             IsInitialized = true;
         }
 
         void OnPaintSample(object sender, SKPaintSurfaceEventArgs e)
         {
-            TextSample.DrawSample(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+            Init();
+            if(Manager.IsValid())
+                TategumiViewCore.DrawHonbunPage(e.Surface.Canvas,Manager.PageList?[0],false);
         }
         void OnTapSample(object sender, EventArgs e)
         {
